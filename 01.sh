805c5990aad2989290d83277fb93948a826d54b8
manudevops90 <manu.devops@gmail.com>
2023-01-26 08:35:34 +0530

    push code

--- 01.sh
+++ 01.sh
@@ -1,5 +1,21 @@
 
-ls
-ls -l
-find /opt
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
 
+yum install mysql-community-server -y
+
+systemctl enable mysqld
+systemctl start mysqld
+
+DEFUAILT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+
+echo MYSQL PASSWORD = $MYSQL_PASSWORD
+echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql --connect-expired-password -uroot -p${DEFUAILT_PASSWORD}
+
+echo "uninstall plugin validate_password;" | mysql -uroot -p$MYSQL_PASSWORD
+
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+
+cd /tmp
+unzip -o mysql.zip
+cd mysql-main
+mysql -u root -pRoboShop@1 <shipping.sql
